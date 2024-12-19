6bc7305129f79d0bbca9280e8cc2b9fd8c4149be
chambit.oh@gmail.com <chambit.oh@gmail.com>
2024-12-19 19:37:38 +0200

    spring mvc study

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -2,6 +2,7 @@ package hello.springmvc.basic.requestmapping;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -49,6 +50,18 @@ public class MappingController {
     @GetMapping(value = "/mapping-header", headers = "mode=debug")
     public String mappingHeader() {
         log.info("mappingHeader");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = MediaType.TEXT_HTML_VALUE)
+    public String mappingProduces() {
+        log.info("mappingProduces");
         return "ok";
     }
 }
